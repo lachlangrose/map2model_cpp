cc0999b472d5b4cbf62fd26d5d5682cf5dca8e46
Yohan de Rose <yohanderose@gmail.com>
2020-07-17 02:59:13 +0000

    origininal functionality restored, if users want to build a standalone m2m that uses the parfile they can. just comment out pybind stuff in map2model.cpp and cmake as usual

--- src/map2model.cpp
+++ src/map2model.cpp
@@ -12,6 +12,8 @@
 #include "tester.h"
 #include "topology_analyzer.h"
 
+#include <pybind11/pybind11.h>
+
 const bool RUN_TESTS = false;
 
 // Main entry point.
@@ -24,29 +26,25 @@ int main(int argc, char *argv[]) {
 
   try {
     // Retrieve parameters filename from a command line.
-    // std::string parfile_path;
-    // if (argc < 2) {
-    //   std::cout << "Error: parameter file is not specified!" << std::endl;
-    //   std::cout << "Usage: map2model <Parfile>" << std::endl;
-    //   exit(0);
-    // } else {
-    //   parfile_path = argv[1];
-    // }
+    std::string parfile_path;
+    if (argc < 2) {
+      std::cout << "Error: parameter file is not specified!" << std::endl;
+      std::cout << "Usage: map2model <Parfile>" << std::endl;
+      exit(0);
+    } else {
+      parfile_path = argv[1];
+    }
 
     // Reading the parameters file.
     ConverterLib::Parameters par;
-    // const std::string parameter_lines = par.Read(parfile_path);
-    const std::string parameter_lines = par.directRead(
-        "../Turner_Syncline/output", "../Turner_Syncline/tmp/hams2_geol.csv",
-        "../Turner_Syncline/tmp/GEOS_GEOLOGY_LINEARSTRUCTURE_500K_GSD.csv",
-        "../Turner_Syncline/tmp/mindeps_2018.csv");
+    const std::string parameter_lines = par.Read(parfile_path);
 
     // Create the output data folder.
     FileUtils::CreateDirectory(par.path_output.c_str());
 
     // Copy parameters file to the output folder (for reference).
-    // FileUtils::CopyFile(parfile_path.c_str(),
-    //                     (par.path_output + "/Parfile").c_str());
+    FileUtils::CopyFile(parfile_path.c_str(),
+                        (par.path_output + "/Parfile").c_str());
 
     //----------------------------------------------------------------------------------
     // Main calculations start here.
@@ -65,4 +63,64 @@ int main(int argc, char *argv[]) {
 
   std::cout << "End." << std::endl;
   return 0;
+}
+
+std::string runMap2Model(const std::string output, const std::string geology,
+                         const std::string faults, const std::string points) {
+
+  std::string result = "";
+
+  try {
+
+    // Reading the parameters file.
+    ConverterLib::Parameters par;
+    const std::string parameter_lines =
+        par.directRead(output, geology, faults, points);
+
+    result += "Successful parameter load\n";
+
+    // // Create the output data folder.
+    FileUtils::CreateDirectory(par.path_output.c_str());
+    result += "Output directory created.\n";
+
+    // Main calculations start here.
+    ConverterLib::TopologyAnalyzer topo_analyzer;
+    topo_analyzer.Initialize(par);
+
+    if (par.subregion_size_x > 0 && par.subregion_size_y > 0) {
+      topo_analyzer.AnalyzeLocalTopology(par);
+    } else {
+      topo_analyzer.AnalyzeGlobalTopology(par, parameter_lines);
+    }
+
+    result += "Calculations done.\n";
+
+  } catch (const std::exception &e) {
+    std::cerr << "Unexpected exception in main caught: " << e.what()
+              << std::endl;
+    result += e.what();
+  }
+
+  return result;
+}
+
+int add(int i, int j) { return i + j; }
+
+// namespace py = pybind11;
+
+// using CLP = ConverterLib::Parameters;
+
+PYBIND11_MODULE(map2model, m) {
+  m.doc() = "pybind11 example plugin"; // optional module docstring
+
+  m.def("add", &add, "A function which adds two numbers");
+  m.def("run", &runMap2Model, "Test function that does nuthin.");
+  // py::class_<TT>(m, "Test").def(py::init()).def("test", &TT::test);
+
+  // py::class_<CLP>(m, "ConverterLibParameters")
+  //     .def(py::init())
+  //     .def("directRead", &CLP::directRead)
+  //     .def_property_readonly("path_output", &CLP::path_output);
+
+  // m.def("run", &run, "Run map2model.");
 }
